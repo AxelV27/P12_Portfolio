b1fa2ec63b2683a1a7990fada010ad198ca544ca
Axel Verdasca <axel.verdasca@numericable.fr>
2024-07-10 11:30:12 +0200

    Porfolio partie 4

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -8,7 +8,7 @@ export default function Apropos (){
             <h1 className="presentation-title">À propos</h1>
                 <div className="presentation-content">
                     <img src={Image} alt="Axel"/>
-                    <p className="presentation-description"> Intégrateur web fraîchement diplômé d'une formation avec Openclassroom. Cette formation de 8 mois s'est composé sous forme de  12 projets à réaliser avec une session de mentorat chaque semaine pour un accompagnement. Mon portfolio regroupe 4 différents projets réalisés lors de ma formation et met en avant mes compétences acquise durant cette formation. Passionné par le développement web et les nouvelles technologies, je suis capable de créer des sites web responsives et intuitifs, mon objectif est de continuer à me former et à me perfectionner dans ce domaine pour proposer des solutions web toujours plus innovantes et performantes.</p>
+                    <p className="presentation-description"> Intégrateur web fraîchement diplômé d'une formation avec Openclassroom. Cette formation de 8 mois s'est composé sous forme de  12 projets à réaliser avec une session de mentorat chaque semaine pour un accompagnement. Mon portfolio regroupe 4 différents projets réalisés lors de ma formation et met en avant mes compétences acquise durant cette formation. Passionné par le développement web et les nouvelles technologies, je suis capable de créer des sites web responsives et intuitifs, mon objectif est de continuer à me former et à me perfectionner dans ce domaine pour proposer des solutions web toujours plus innovantes et performantes.Vous pouvez retrouver mon CV juste <a href="https://1drv.ms/b/s!Avl3N-YbY3A8iX1_Dvffbe3ZZ_5L?e=cbmtlK">ICI</a></p>
                 </div>
         </section>
 
